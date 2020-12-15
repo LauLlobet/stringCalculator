31ca47490e715058d951257f14059ff511053d4c
Lau Llobet <lau.llobet@codurance.com>
2020-12-15 14:20:30 +0100

    🟢🟢🟢🟢 GREEN - bussines requirement done - to squash

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,8 @@ export function calculator(string : string): number {
     const commaSeparatedString = separateWithCommas(string)
     if(commaSeparatedString.includes("-")){
         const stringWithoutNonNegatives = string.slice(commaSeparatedString.indexOf('-')+1)
-        const negative = "-" + stringWithoutNonNegatives.split("-")[0].split(",")[0]
-        throw new Error("negatives "+ negative + " found")
+        const negative = stringWithoutNonNegatives.split("-").reduce((accum, current)=> accum + " -" + current.split(",")[0], "")
+        throw new Error("negatives"+ negative + " found")
     }
     if(isInvalidlySeparated(commaSeparatedString)){ 
         throw new Error('not well separated');
