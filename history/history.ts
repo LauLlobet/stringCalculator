6d47b9eebeafab0708cd6c3d2798f66d8d3b6274
Lau Llobet <lau.llobet@codurance.com>
2020-12-15 12:32:14 +0100

    🟢🟢🟢🟢 GREEN - fake impl - to squash

--- history/history.ts
+++ history/history.ts
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 
 describe('sample test-suite', () => {
     it('sum 1, 3 should be 4', () => {
-        expect(sum("1, 3")).eql(4);
+        expect(sum("1,3")).eql(4);
     });
 });
 
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,6 +3,6 @@ import { expect } from 'chai';
 
 describe('sample test-suite', () => {
     it('sum 1, 3 should be 4', () => {
-        expect(sum("1, 3")).eql(4);
+        expect(sum("1,3")).eql(4);
     });
 });
