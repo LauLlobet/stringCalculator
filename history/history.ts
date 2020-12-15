901353ea538b12931829d6ddf32c418650686ec6
Lau Llobet <lau.llobet@codurance.com>
2020-12-15 12:34:52 +0100

    🔴🔴🔴🔴 RED -  - to squash

--- history/history.ts
+++ history/history.ts
@@ -4,6 +4,7 @@ import { expect } from 'chai';
 describe('calculator should', () => {
     it('sum values with comma', () => {
         expect(calculator("1,3")).eql(4);
+        expect(calculator("2,3")).eql(5);
     });
 });
 
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,5 +4,6 @@ import { expect } from 'chai';
 describe('calculator should', () => {
     it('sum values with comma', () => {
         expect(calculator("1,3")).eql(4);
+        expect(calculator("2,3")).eql(5);
     });
 });
