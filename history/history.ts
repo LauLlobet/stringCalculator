c4f90c72f42de76f67ead9dbe46de5cb7e297f2e
Lau Llobet <lau.llobet@codurance.com>
2020-12-15 13:44:18 +0100

    👍👍👍👍 REFACTOR - Reflect single responsability in each function module - to squash

--- history/history.ts
+++ history/history.ts
@@ -31,13 +31,18 @@ describe('calculator should', () => {
 function isInvalidlySeparated(str: string): boolean {
     return str.endsWith(',') || str.includes(",,") || str.startsWith(",")
 }
-export function calculator(str : string): number {
-    if(isInvalidlySeparated(str)){ 
+
+function separateWithCommas(str: string): string {
+    const separator = str.split("\n")[0].split("//")[1]
+    return str.replace("//"+separator+"\n","").replace(new RegExp(separator,'g'),',')
+}
+
+export function calculator(string : string): number {
+    if(isInvalidlySeparated(string)){ 
          throw new Error('not well separated');
     }
-    const separator = str.split("\n")[0].split("//")[1]
-    const commaSeparatedString = str.replace("//"+separator+"\n","").replace(new RegExp(separator,'g'),',')
-    return commaSeparatedString
-    .split(',')
+    const commaSeparatedString = separateWithCommas(string)
+    
+    return commaSeparatedString.split(',')
     .reduce((accum,current) => accum + +current, 0)
 }
