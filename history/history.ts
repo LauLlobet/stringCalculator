f8099646de1a87e03d67f435060369c8ef257819
Lau Llobet <lau.llobet@codurance.com>
2020-12-15 12:46:21 +0100

    🟢🟢🟢🟢 GREEN - our logic was so generic, we can't go red for next bussines requirement - to squash

--- history/history.ts
+++ history/history.ts
@@ -6,6 +6,9 @@ describe('calculator should', () => {
         expect(calculator("1,3")).eql(4);
         expect(calculator("2,3")).eql(5);
     });
+    it('empty string is zero'), () => {
+        expect(calculator("")).eql(0);
+    }
 });
 
 //-------- IMPLEMENTATION --------
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -6,4 +6,7 @@ describe('calculator should', () => {
         expect(calculator("1,3")).eql(4);
         expect(calculator("2,3")).eql(5);
     });
+    it('empty string is zero'), () => {
+        expect(calculator("")).eql(0);
+    }
 });
