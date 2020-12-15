dacf13fae388062539cfa058c81f5775e9b83c1d
Lau Llobet <lau.llobet@codurance.com>
2020-12-15 12:59:15 +0100

    🟢🟢🟢🟢 GREEN - fixing skiped tests - to squash

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -6,10 +6,10 @@ describe('calculator should', () => {
         expect(calculator("1,3")).eql(4);
         expect(calculator("2,3")).eql(5);
     });
-    it('empty string is zero'), () => {
+    it('empty string is zero', () => {
         expect(calculator("")).eql(0);
-    }
-    it('sum an indefinate number of numbers'), () => {
-        expect(calculator("1,2,3,4,5,6,7,8,9")).eql(45)
-    }
+    });
+    it('sum an indefinate number of numbers', () => {
+        expect(calculator("1,2,3,4,5,6,7,8,9")).eql(45);
+    });
 });
